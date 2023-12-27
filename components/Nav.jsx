bfa4207defdfa13b2e7f312ab58b162279689dae
El-Collins <charlescollins476@gmail.com>
2023-12-27 20:06:25 +0100

    Complete moble navigation bar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,8 +6,9 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = false;
+  const isUserLoggedIn = true;
   const [providers, setProviders] = useState(null);
+  const [toggleDropDown, setToggleDropDown] = useState(false);
 
   useEffect(() => {
     const setProviders = async () => {
@@ -54,13 +55,58 @@ const Nav = () => {
           </div>
         ) : (
           <>
-          {
-            providers && Object.values(providers).map((provider) => (
-              <button type="button" key={provider.name} onClick={()=> signIn(provider.id)} className="black_btn">
-                Sign In
-              </button>
-            ))
-          }
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+      {/* Moblie Navigation */}
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image
+              src="/assets/images/profile.png"
+              alt="profile-image"
+              width={37}
+              height={37}
+              className="rounded-full outline" onClick={()=> setToggleDropDown((prev) => !prev)
+              }
+            />
+            {toggleDropDown && (<div className="dropdown">
+              <Link href='/profile' className="dropdown_link" onClick={()=> setToggleDropDown(false)}>
+              My Profle
+              </Link>
+              <Link href='/create-prompt' className="dropdown_link" onClick={()=> setToggleDropDown(false)}>
+              Create Prompt
+              </Link>
+              <button type="button" onClick={()=>{
+                setToggleDropDown(false);
+                signOut();
+              }} className="mt-5 w-full black_btn">Sign Out</button>
+            </div>) }
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
           </>
         )}
       </div>
