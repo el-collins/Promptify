8fd635205284f6cbcd86a2dc11f0ed5c25e0afae
El-Collins <charlescollins476@gmail.com>
2023-12-28 14:49:47 +0100

    complete handle delete functionality

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation";
 import Profile from "@components/profile";
 
 const MyProfile = () => {
-    const router = useRouter();
+  const router = useRouter();
   const { data: session } = useSession();
   const [posts, setPosts] = useState([]);
 
@@ -21,10 +21,27 @@ const MyProfile = () => {
   }, []);
 
   const handleEdit = (post) => {
-   router.push(`/update-prompt?id=${post._id}`)
+    router.push(`/update-prompt?id=${post._id}`);
   };
 
-  const handleDelete = async (post) => {};
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, { method: "DELETE" });
+
+        const filteredPosts = posts.filter((post) => post._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <Profile
       name="My"
